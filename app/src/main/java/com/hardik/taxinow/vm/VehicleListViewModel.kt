0cd69dd1a24984182dce2774f2cdfabe746dfb60
Hardik Bambhania <hardik.p.bambhania@gmail.com>
2022-01-19 23:32:23 +0530

    Added first empty UI.

---
+++ app/src/main/java/com/hardik/taxinow/vm/VehicleListViewModel.kt
@@ -0,0 +1,10 @@
+package com.hardik.taxinow.vm
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class VehicleListViewModel @Inject constructor() : ViewModel() {
+
+}

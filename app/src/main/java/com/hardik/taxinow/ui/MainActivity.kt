a51927b084dcf7187fa49af37e33d9deccde9f98
Hardik Bambhania <hardik.p.bambhania@gmail.com>
2022-01-24 14:51:26 +0530

    Added app logo and splash screen

--- app/src/main/java/com/hardik/taxinow/ui/MainActivity.kt
+++ app/src/main/java/com/hardik/taxinow/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.hardik.taxinow.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.hardik.taxinow.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -9,6 +10,10 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Handle the splash screen transition.
+        installSplashScreen()
+
         setContentView(R.layout.activity_main)
     }
 }
